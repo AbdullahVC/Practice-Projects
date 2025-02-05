047721705fb401a8dd6a021a2f868cc68c6ec3a9
Abdullah Veysel Çoşkun <abdullahveyselcoskun@gmail.com>
2025-02-05 19:36:11 +0300

    played around

--- JS/Test/main.js
+++ JS/Test/main.js
@@ -1,40 +1,12 @@
-const button = document.querySelector("#colorButton");
-const box = document.querySelector("#colorBox");
-const img = document.querySelector("img");
-button.innerHTML = "Bas bakalım";
+const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+const password = document.getElementById("password");
+const check = document.getElementById("check-password");
+const result = document.getElementById("result");
 
-function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-let isBig = false;
-let clickCount = 0;
-
-button.addEventListener("click", () => {
-  clickCount++;
-
-  if (clickCount === 5) {
-    img.style.cssText = `
-        display: block;
-        width: 100%;
-        height: 100vh;`;
-  }
-
-  const randomColor = getRandomColor();
-  box.style.backgroundColor = randomColor;
-  if (isBig) {
-    // Küçült
-    box.style.width = "100px";
-    box.style.height = "100px";
+check.addEventListener("click", () => {
+  if (regex === password.value) {
+    result.textContent = "Password is valid";
   } else {
-    // Büyüt
-    box.style.width = "200px";
-    box.style.height = "200px";
+    result.textContent = "Password is invalid";
   }
-  isBig = !isBig;
 });
