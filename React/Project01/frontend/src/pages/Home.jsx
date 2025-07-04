37774068e87e77c76bf8dbb2f0d831a116fb00c0
Abdullah Veysel Çoşkun <abdullahvcoskun@gmail.com>
2025-07-04 17:20:51 +0300

    devam

--- React/Project01/frontend/src/pages/Home.jsx
+++ React/Project01/frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "John Wick", release_date: "2014" },
     { id: 2, title: "The Matrix", release_date: "1999" },
@@ -8,9 +11,24 @@ function Home() {
     { id: 4, title: "Interstellar", release_date: "2014" },
   ];
 
+  const handleSearch = () => {
+    alert(searchQuery);
+  };
+
   return (
-    <div className="home">0
-        <form onSubmit = {} className="search-form"></form>
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
       <div className="movies-grid">
         {movies.map((movie) => (
           <MovieCard movie={movie} key={movie.id} />
@@ -21,13 +39,3 @@ function Home() {
 }
 
 export default Home;
-
-
-
-
-
-
-
-
-
- Dakka 39:47
